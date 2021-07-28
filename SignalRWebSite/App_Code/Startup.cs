b0003a78edbb107e132a7e0783d7cd3806b6bbd8
Chris Wong <wc38.wong@gmail.com>
2021-07-28 16:42:58 +0800

    Tutorial: Real-time chat with SignalR 2

---
+++ SignalRWebSite/App_Code/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+public class Startup
+{
+    public void Configuration(IAppBuilder app)
+    {
+        // Any connection or hub wire up and configuration should go here
+        app.MapSignalR();
+    }
+}
